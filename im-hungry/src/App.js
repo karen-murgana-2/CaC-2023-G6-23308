2c0f4ef245e67c2b6a7ab17428ce1348f1e64162
LucasJoelMcG <93175256+LucasJoelMcG@users.noreply.github.com>
2023-06-06 22:41:07 -0300

    api consumption example ready

--- im-hungry/src/App.js
+++ im-hungry/src/App.js
@@ -1,9 +1,28 @@
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  const [meals, setMeals] = useState([]);
+  const url = "https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata";
+
+  useEffect(() => {
+    const fetchMeals = async () => {
+      try {
+        const response = await axios.get(url);
+        setMeals(response.data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+    fetchMeals();
+  }, []);
+
+  if (meals.length === 0) return;
+
   return (
     <div className="App">
-    
+      <p>{meals.meals[0].strInstructions}</p>
     </div>
   );
 }
